7ad52003a4915b3f009cb333ab7583b870729b3b
DiceglieA <angelodiceglie10@gmail.com>
2023-03-22 14:31:47 +0100

    fino a moltiplicazione

--- CalcoliHelper.cs
+++ CalcoliHelper.cs
@@ -5,6 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 public class CalcoliHelper
 {
-   
+    public static int SommaInteri(int num1, int num2) => num1 * num2;
+    public static double SommaDecimali(double num1, double num2) => num1 * num2;
+
+    public static int DifferenzaInteri(int num1, int num2) => num1 - num2;
+    public static double DifferenzaDecimali(double num1, double num2) => num1 - num2;
+
+    public static int MoltiplicazioneInteri(int num1, int num2) => num1 * num2;
+    public static double MoltiplicazioneDecimali(double num1, double num2) => num1 * num2;
+
+    public static
+
+
+
 }
 
